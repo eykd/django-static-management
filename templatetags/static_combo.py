4b08c53c723cf23294a4dda1cb427428d8165375
deyk <deyk@crossway.org>
2010-07-12 16:35:26 -0500

    Adding titles to emitted link elements.

--- templatetags/static_combo.py
+++ templatetags/static_combo.py
@@ -7,19 +7,21 @@ from django.conf import settings
 register = template.Library()
 
 @register.simple_tag
-def static_combo_css(file_name, media='all', alternate=False):
+def static_combo_css(file_name, media='all', title='', alternate=False):
     """combines files in settings
     
     {% static_combo_css "css/main.css" %}"""
     # override the default if an override exists
     alt = alternate and "alternate " or ""
+    title = title and ('title="%s" ' % title) or ''
     try:
-        link_format = settings.STATIC_MANAGEMENT_CSS_LINK % {'href': "%s", 'media': media, 'alt': alt}
+        link_format = settings.STATIC_MANAGEMENT_CSS_LINK % {'href': "%s", 'media': media, 'alt': alt, 'title': title}
     except AttributeError:
-        link_format = '<link rel="%(alt)sstylesheet" type="text/css" href="%(href)s" media="%(media)s">\n' % {
+        link_format = '<link rel="%(alt)sstylesheet" type="text/css" href="%(href)s" media="%(media)s" %(title)s>\n' % {
             'alt': alt, 
             'href': "%s", 
-            'media': media
+            'media': media,
+            'title': title,
             }
     output = _group_file_names_and_output(file_name, link_format, 'css')
     return output
